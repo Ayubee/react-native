3b666381fef63f39a7850d9d1d3717a21a09653a
Ayubeey <aubsattorov@gmail.com>
2022-03-29 17:39:54 +0300

    problem with alert

--- my-app/App.js
+++ my-app/App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Button, Alert } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image, TouchableWithoutFeedback} from 'react-native';
 import React from 'react';
 
 export default function App() {
 
-  const click =()=> console.log('clisk work!')
-  const title =()=> Alert.alert("H1","p" ,[
-    {text: "yes"},
-    {text: "no"}
-  ]);
+
+  const clickme =()=> console.log('clisk work!');
+  const title =()=> Alert.arelt("My app", "title");
+  const clicktest =()=> console.log('clisk test work')
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} style={styles.text} onPress={click} >lorema</Text>
-    <Button title={'click'}color='red' onPress={title}/>
+      <Text numberOfLines={2} style={styles.text} onPress={clickme} >lorema</Text>
+    <Button title={'click'} color='red' onPress={title}/>
+    <Button title={'click test'} color='red' onPress={clicktest}/>
+
+    <TouchableWithoutFeedback onPress={clicktest}>
+    <Image blurRadius={2} source={{
+      width:300,
+      height:150,
+      uri:"https://images.wallpaperscraft.ru/image/single/bashnia_zdaniia_steklo_269740_1920x1080.jpg"
+    }}/>
+</TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
